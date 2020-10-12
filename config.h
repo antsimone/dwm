c8ecb806e13c1cb04ee761fe6d8974255cc2bf02
Antonio Simone <antsimone03@gmail.com>
2020-10-13 00:17:43 +0200

    custom functions in .h, fix alignment

--- config.h
+++ config.h
@@ -24,8 +24,8 @@ static const char col_selborder[]  = "#0066ff";
 
 /* -- fg - bg - border -- */
 static const char *colors[][3]     = {
-    [SchemeNorm] = { col_normfg, col_normbg, col_normborder },
-    [SchemeSel]  = { col_selfg, col_selbg, col_selborder},
+	[SchemeNorm] = { col_normfg, col_normbg, col_normborder },
+	[SchemeSel]  = { col_selfg, col_selbg, col_selborder},
 };
 
 /* -- tags -- */
@@ -33,22 +33,21 @@ static const char *tags[] = { "1", "2", "3", "4" };
 
 /* -- layouts -- */
 static const Layout layouts[] = {
-    /* symbol     arrange function */
-    { "[]=",      tile },       /* first entry is default */
-    { "[M]",      monocle }, 
-    { "><>",      NULL },       /* no layout function means floating behavior */
+	/* symbol     arrange function */
+	{ "[]=",      tile },       /* first entry is default */
+	{ "[M]",      monocle }, 
+	{ "><>",      NULL },       /* no layout function means floating behavior */
 };
 
 /* -- window rules -- */
 static const Rule rules[] = {
-    /* xprop(1):
-     * WM_CLASS(STRING) = instance, class
-     * WM_NAME(STRING) = title
-     */
-    /* class      instance    title       tags mask    iscentered    isfloating   monitor */
-    { "Gimp"    , NULL,       NULL,       0,            0,          1,           -1 },
-    { "Leafpad" , NULL,       NULL,       0,            1,          1,           -1 },
-
+	/* xprop(1):
+	 * WM_CLASS(STRING) = instance, class
+	 * WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask    iscentered    isfloating   monitor */
+	{ "Gimp"    , NULL,       NULL,       0,            0,          1,           -1 },
+	{ "Leafpad" , NULL,       NULL,       0,            1,          1,           -1 },
 };
 
 /* -- key definitions -- */
@@ -58,6 +57,8 @@ static const Rule rules[] = {
 { MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
 { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* -- added keysym -- */
 #define XF86XK_MonBrightnessDown 0x1008ff03
 #define XF86XK_MonBrightnessUp   0x1008ff02
 #define XF86XK_AudioMute    	 0x1008ff12
@@ -73,63 +74,129 @@ static const char *dmenucmd[] = { "dmenu_run", NULL};
 static const char *termcmd[]  = { "st", NULL };
 static const char *firefox[]  = { "firefox", NULL };
 
-/* -- manual patches -- */
+/* -- custom functions declarations -- */
 static void shiftview(const Arg *arg); /* shift tags view */
 static void movestack(const Arg *arg); /* rotate windows in stack */
 
 /* -- key bindings --*/
 static Key keys[] = {
-    TAGKEYS(XK_1       , 0 )
-        TAGKEYS(XK_2       , 1 )
-        TAGKEYS(XK_3       , 2 )
-        TAGKEYS(XK_4       , 3 )
-        /* modifier                     key        function        argument */
-        { MODKEY|ShiftMask , XK_period                  ,spawn,          SHCMD("dmenu_shutdown"                                          ) },
-        { 0                , XF86XK_MonBrightnessUp     ,spawn,          SHCMD("xbacklight -inc 10"                                      ) },
-        { 0                , XF86XK_MonBrightnessDown   ,spawn,          SHCMD("xbacklight -dec 10"                                      ) },
-        { 0                , XF86XK_AudioMute           ,spawn,          SHCMD("pamixer -t && pkill -RTMIN+3 dwmblocks"                  ) },
-        { 0                , XF86XK_AudioLowerVolume    ,spawn,          SHCMD("pamixer -u && pamixer --allow-boost -d 10 && pkill -RTMIN+3 dwmblocks" ) },
-        { 0                , XF86XK_AudioRaiseVolume    ,spawn,          SHCMD("pamixer -u && pamixer --allow-boost -i 10 && pkill -RTMIN+3 dwmblocks" ) },
-        { MODKEY           , XK_period                  ,spawn,          {.v = dmenucmd    } },
-        { MODKEY           , XK_Return                  ,spawn,          {.v = termcmd     } },
-        { MODKEY           , XK_comma                   ,spawn,          {.v = firefox     } },
-        { MODKEY           , XK_b                       ,togglebar,      {0                } },
-        { MODKEY           , XK_j                       ,focusstack,     {.i = +1          } },
-        { MODKEY           , XK_k                       ,focusstack,     {.i = -1          } },
-        { MODKEY           , XK_i                       ,incnmaster,     {.i = +1          } },
-        { MODKEY           , XK_d                       ,incnmaster,     {.i = -1          } },
-        { MODKEY           , XK_h                       ,setmfact,       {.f = -0.05       } },
-        { MODKEY           , XK_l                       ,setmfact,       {.f = +0.05       } },
-        { MODKEY|ShiftMask , XK_Return                  ,zoom,           {0                } },
-        { MODKEY           , XK_Tab                     ,view,           {0                } },
-        { MODKEY           , XK_c                       ,killclient,     {0                } },
-        { MODKEY           , XK_t                       ,setlayout,      {.v = &layouts[0] } },
-        { MODKEY           , XK_f                       ,setlayout,      {.v = &layouts[2] } },
-        { MODKEY           , XK_m                       ,setlayout,      {.v = &layouts[1] } },
-        { MODKEY           , XK_n                       ,shiftview,      {.i = +1          } },
-        { MODKEY           , XK_p                       ,shiftview,      {.i = -1          } },
-        { MODKEY|ShiftMask , XK_j                       ,movestack,      {.i = +1          } },
-        { MODKEY|ShiftMask , XK_k                       ,movestack,      {.i = -1          } },
-        { MODKEY           , XK_space                   ,setlayout,      {0                } },
-        { MODKEY|ShiftMask , XK_space                   ,togglefloating, {0                } },
-        { MODKEY           , XK_0                       ,view,           {.ui = ~0         } },
-        { MODKEY|ShiftMask , XK_0                       ,tag,            {.ui = ~0         } },
-        { MODKEY|ShiftMask , XK_comma                   ,tagmon,         {.i = -1          } },
-        { MODKEY|ShiftMask , XK_q                       ,quit,           {0                } },
-
+    /* modifier                     key        function        argument */
+        TAGKEYS(            XK_1,                                 0 )
+        TAGKEYS(            XK_2,                                 1 )
+        TAGKEYS(            XK_3,                                 2 )
+        TAGKEYS(            XK_4,                                 3 )
+        { MODKEY|ShiftMask, XK_period,                spawn,      SHCMD("dmenu_shutdown") },
+        { 0,                XF86XK_MonBrightnessUp,   spawn,      SHCMD("xbacklight -inc 10") },
+        { 0,                XF86XK_MonBrightnessDown, spawn,      SHCMD("xbacklight -dec 10") },
+        { 0,                XF86XK_AudioMute,         spawn,      SHCMD("pamixer -t && pkill -RTMIN+3 dwmblocks") },
+        { 0,                XF86XK_AudioLowerVolume,  spawn,      SHCMD("pamixer -u && pamixer --allow-boost -d 10 && pkill -RTMIN+3 dwmblocks") },
+        { 0,                XF86XK_AudioRaiseVolume,  spawn,      SHCMD("pamixer -u && pamixer --allow-boost -i 10 && pkill -RTMIN+3 dwmblocks") },
+        { 0,                XK_Print,                 spawn,      SHCMD("maim -u ~/pics/$(date +%s).png" ) },
+        { MODKEY,           XK_Print,                 spawn,      SHCMD("maim -s -u | xclip -selection clipboard -t image/png -i" ) },
+        { MODKEY,           XK_period,                spawn,      {.v = dmenucmd } },
+        { MODKEY,           XK_Return,                spawn,      {.v = termcmd } },
+        { MODKEY,           XK_comma,                 spawn,      {.v = firefox } },
+        { MODKEY,           XK_t,                     setlayout,  {.v = &layouts[0] } },
+        { MODKEY,           XK_f,                     setlayout,  {.v = &layouts[2] } },
+        { MODKEY,           XK_m,                     setlayout,  {.v = &layouts[1] } },
+        { MODKEY,           XK_space,                 setlayout,  {0 } },
+        { MODKEY,           XK_n,                     shiftview,  {.i = +1 } },
+        { MODKEY,           XK_p,                     shiftview,  {.i = -1 } },
+        { MODKEY,           XK_j,                     focusstack, {.i = +1 } },
+        { MODKEY,           XK_k,                     focusstack, {.i = -1 } },
+        { MODKEY|ShiftMask, XK_j,                     movestack,  {.i = +1 } },
+        { MODKEY|ShiftMask, XK_k,                     movestack,  {.i = -1 } },
+        { MODKEY,           XK_h,                     setmfact,   {.f = -0.05 } },
+        { MODKEY,           XK_l,                     setmfact,   {.f = +0.05 } },
+        { MODKEY,           XK_i,                     incnmaster, {.i = +1 } },
+        { MODKEY,           XK_d,                     incnmaster, {.i = -1 } },
+        { MODKEY,           XK_b,                     togglebar,  {0 } },
+        { MODKEY,           XK_c,                     killclient, {0 } },
+        { MODKEY,           XK_0,                     view,       {.ui = ~0 } },
+        { MODKEY,           XK_Tab,                   view,       {0 } },
+        { MODKEY|ShiftMask, XK_Return,                zoom,       {0 } },
+        { MODKEY|ShiftMask, XK_0,                     tag,        {.ui = ~0 } },
+        { MODKEY|ShiftMask, XK_comma,                 tagmon,     {.i = -1 } },
+        { MODKEY|ShiftMask, XK_q,                     quit,       {0 } },
 };
 
 /* -- buttons -- */
 static Button buttons[] = {
-    /* click                event mask      button          function        argument */
-    { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-    { ClkWinTitle,          0,              Button1,        zoom,           {0} },
-    { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-    { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    { ClkTagBar,            0,              Button1,        toggleview,     {0} },
-    { ClkTagBar,            0,              Button3,        view,           {0} },
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button1,        zoom,           {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        toggleview,     {0} },
+	{ ClkTagBar,            0,              Button3,        view,           {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
+/* -- custom functions -- */
+void
+movestack(const Arg *arg) {
+	Client *c = NULL, *p = NULL, *pc = NULL, *i;
+
+	if(arg->i > 0) {
+		/* find the client after selmon->sel */
+		for(c = selmon->sel->next; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+		if(!c)
+			for(c = selmon->clients; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+
+	}
+	else {
+		/* find the client before selmon->sel */
+		for(i = selmon->clients; i != selmon->sel; i = i->next)
+			if(ISVISIBLE(i) && !i->isfloating)
+				c = i;
+		if(!c)
+			for(; i; i = i->next)
+				if(ISVISIBLE(i) && !i->isfloating)
+					c = i;
+	}
+	/* find the client before selmon->sel and c */
+	for(i = selmon->clients; i && (!p || !pc); i = i->next) {
+		if(i->next == selmon->sel)
+			p = i;
+		if(i->next == c)
+			pc = i;
+	}
+
+	/* swap c and selmon->sel selmon->clients in the selmon->clients list */
+	if(c && c != selmon->sel) {
+		Client *temp = selmon->sel->next==c?selmon->sel:selmon->sel->next;
+		selmon->sel->next = c->next==selmon->sel?c:c->next;
+		c->next = temp;
+
+		if(p && p != c)
+			p->next = c;
+		if(pc && pc != selmon->sel)
+			pc->next = selmon->sel;
+
+		if(selmon->sel == selmon->clients)
+			selmon->clients = c;
+		else if(c == selmon->clients)
+			selmon->clients = selmon->sel;
+
+		arrange(selmon);
+	}
+}
+
+void
+shiftview(const Arg *arg) {
+	Arg shifted;
+
+	/* left circular shift */
+	if(arg->i > 0) 
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+			| (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+	/* right circular shift */
+	else 
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+			| selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	view(&shifted);
+}
