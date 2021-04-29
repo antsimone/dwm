fb49ac1eef8c894c8ce4e087c2cde9fa0327adcf
Antonio Simone <antsimone03@gmail.com>
2021-04-29 19:03:23 +0200

    same with tempus fg

--- config.h
+++ config.h
@@ -1,36 +1,37 @@
 /* See LICENSE file for copyright and license details. */
 
-/* window sizing */
+/* Appearance */
+
+static const int nmaster           = 1;    /* number of clients in master area */
+static const int resizehints       = 0;    /* 1 means respect size hints in tiled resizals */
+static const float mfact           = 0.55; /* factor of master area size [0.05..0.95] */
 static const unsigned int borderpx = 1;    /* border pixel of windows */
 static const unsigned int snap     = 32;   /* snap pixel */
 static const unsigned int gappx    = 0;    /* gap pixel between windows */
-static const float mfact           = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster           = 1;    /* number of clients in master area */
-static const int resizehints       = 0;    /* 1 means respect size hints in tiled resizals */
 
-/* bar */
 static const int showbar           = 1; /* 0 means no bar */
 static const int topbar            = 1; /* 0 means bottom bar */
+
 static const char *fonts[]         = { "monospace:pixelsize=12" };
 
-/* colors */
-static const char col_normfg[]     = "#969696";
-static const char col_normbg[]     = "#121212";
-static const char col_normborder[] = "#121212";
-static const char col_selfg[]      = "#d89333";
-static const char col_selbg[]      = "#232323";
+static const char col_normfg[]     = "#bcbcbc"; /* "#969696" */
+static const char col_normbg[]     = "#121212"; /* "#232323" */
+static const char col_normborder[] = "#232323";
+static const char col_selfg[]      = "#d0913d"; /* "#dd8844" "#d89333" */
+static const char col_selbg[]      = "#232323"; /* "#1b1d24" "#2a2e38" */
 static const char col_selborder[]  = "#446fbd";
 
 static const char *colors[][3] = {
-  [SchemeNorm] = { col_normfg, col_normbg, col_normborder },
-  [SchemeSel]  = { col_selfg, col_selbg,  col_selborder  },
+    [SchemeNorm] = { col_normfg, col_normbg, col_normborder },
+    [SchemeSel]  = { col_selfg, col_selbg,  col_selborder  },
 };
 
-/* tagging */
+/* Tags */
+
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
+/* Layouts */
 
-/* layouts */
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },       /* first entry is default */
@@ -38,7 +39,7 @@ static const Layout layouts[] = {
     { "><>",      NULL },       /* no layout function means floating behavior */
 };
 
-/* window rules */
+/* Window rules */
 static const Rule rules[] = {
     /* xprop(1):
      * WM_CLASS(STRING) = instance, class
@@ -48,7 +49,8 @@ static const Rule rules[] = {
     { "Gimp"    , NULL,       NULL,       0,            0,          1,           -1 },
 };
 
-/* key definitions */
+/* Key definitions */
+
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 { MODKEY,                       KEY, view, {.ui = 1 << TAG} }, \
@@ -56,40 +58,43 @@ static const Rule rules[] = {
 { MODKEY|ShiftMask,             KEY, tag, {.ui = 1 << TAG} }, \
 { MODKEY|ControlMask|ShiftMask, KEY, toggletag, {.ui = 1 << TAG} },
 
-/* keysym */
 #define XF86XK_MonBrightnessDown 0x1008ff03
 #define XF86XK_MonBrightnessUp   0x1008ff02
 #define XF86XK_AudioMute    	 0x1008ff12
 #define XF86XK_AudioLowerVolume  0x1008ff11
 #define XF86XK_AudioRaiseVolume  0x1008ff13
 
-/* shell commands */
-#define ShCmd(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+/* Shell commands */
 
-/* commands */
-static char dmenumon[2]       = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL};
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define VOL_MUTE "pamixer -t && pkill -RTMIN+3 dwmblocks"
+#define VOL_LOWER "pamixer -u && pamixer --allow-boost -d 10 && pkill -RTMIN+3 dwmblocks"
+#define VOL_RAISE "pamixer -u && pamixer --allow-boost -i 10 && pkill -RTMIN+3 dwmblocks"
+#define SCREEN "maim -u ~/pics/$(date +%s).png"
+#define SCREEN_CPY "maim -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i"
+
+/* Programs and commands */
+
+static char dmenumon[2]       = "0";
+static const char *dmenucmd[] = { "dmenu_run", "-nb", col_normbg, "-nf", col_normfg, "-sb", \
+    col_selbg, "-sf", col_selfg, NULL};
 static const char *shut[]     = { "dmenu_shutdown", NULL };
 static const char *term[]     = { "st", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char *fm[]       = { "pcmanfm", NULL };
-static const char *maim[]     = { "maim", "-u", "~/pics/$(date +%s).png", NULL };
 static const char *xbinc[]    = { "xbacklight", "-inc", "10", NULL };
 static const char *xbdec[]    = { "xbacklight", "-dec", "10", NULL };
 static const char *emacs[]    = { "emacsclient", "-ca", "", NULL };
 
-#define VolMuteCmd "pamixer -t && pkill -RTMIN+3 dwmblocks"
-#define VolLowerCmd "pamixer -u && pamixer --allow-boost -d 10 && pkill -RTMIN+3 dwmblocks"
-#define VolRaiseCmd "pamixer -u && pamixer --allow-boost -i 10 && pkill -RTMIN+3 dwmblocks"
-#define ScreenCpyCmd "maim -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i"
+/* Declare custom functions */
 
-/* custom functions declarations */
 static void shiftview(const Arg *arg); /* shift tags view */
 static void movestack(const Arg *arg); /* rotate windows in stack */
 
-/* key bindings --*/
+/* Key bindings */
+
 static Key keys[] = {
-  /* modifier                     key        function        argument */
+  /* modifier         key        function        argument */
   TAGKEYS(            XK_1,                                 0 )
   TAGKEYS(            XK_2,                                 1 )
   TAGKEYS(            XK_3,                                 2 )
@@ -98,11 +103,11 @@ static Key keys[] = {
   { MODKEY|ShiftMask, XK_period,                spawn,      {.v = shut} },
   { 0,                XF86XK_MonBrightnessUp,   spawn,      {.v = xbinc} },
   { 0,                XF86XK_MonBrightnessDown, spawn,      {.v = xbdec} },
-  { 0,                XF86XK_AudioMute,         spawn,      ShCmd(VolMuteCmd) },
-  { 0,                XF86XK_AudioLowerVolume,  spawn,      ShCmd(VolLowerCmd) },
-  { 0,                XF86XK_AudioRaiseVolume,  spawn,      ShCmd(VolRaiseCmd) },
-  { 0,                XK_Print,                 spawn,      {.v = maim} },
-  { MODKEY,           XK_Print,                 spawn,      ShCmd(ScreenCpyCmd)},
+  { 0,                XF86XK_AudioMute,         spawn,      SHCMD(VOL_MUTE) },
+  { 0,                XF86XK_AudioLowerVolume,  spawn,      SHCMD(VOL_LOWER) },
+  { 0,                XF86XK_AudioRaiseVolume,  spawn,      SHCMD(VOL_RAISE) },
+  { 0,                XK_Print,                 spawn,      SHCMD(SCREEN)},
+  { MODKEY,           XK_Print,                 spawn,      SHCMD(SCREEN_CPY)},
   { MODKEY,           XK_period,                spawn,      {.v = dmenucmd} },
   { MODKEY,           XK_Return,                spawn,      {.v = term} },
   { MODKEY,           XK_e,                     spawn,      {.v = emacs} },
@@ -129,7 +134,7 @@ static Key keys[] = {
   { MODKEY|ShiftMask, XK_q,                     quit,       {0 } },
 };
 
-/* buttons */
+/* Buttons */
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
     { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
@@ -144,20 +149,17 @@ static Button buttons[] = {
     { ClkTagBar,            MODKEY,         Button3,        tag,            {0} },
 };
 
-/* custom functions */
+
 void
 movestack(const Arg *arg) {
     Client *c = NULL, *p = NULL, *pc = NULL, *i;
 
     if(arg->i > 0) {
-        /* find the client after selmon->sel */
         for(c = selmon->sel->next; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
         if(!c)
             for(c = selmon->clients; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
-
     }
     else {
-        /* find the client before selmon->sel */
         for(i = selmon->clients; i != selmon->sel; i = i->next)
             if(ISVISIBLE(i) && !i->isfloating)
                 c = i;
@@ -166,7 +168,6 @@ movestack(const Arg *arg) {
                 if(ISVISIBLE(i) && !i->isfloating)
                     c = i;
     }
-    /* find the client before selmon->sel and c */
     for(i = selmon->clients; i && (!p || !pc); i = i->next) {
         if(i->next == selmon->sel)
             p = i;
@@ -174,7 +175,7 @@ movestack(const Arg *arg) {
             pc = i;
     }
 
-    /* swap c and selmon->sel selmon->clients in the selmon->clients list */
+    /* Swap c and selmon->sel selmon->clients in the selmon->clients list */
     if(c && c != selmon->sel) {
         Client *temp = selmon->sel->next==c?selmon->sel:selmon->sel->next;
         selmon->sel->next = c->next==selmon->sel?c:c->next;
@@ -198,11 +199,11 @@ void
 shiftview(const Arg *arg) {
     Arg shifted;
 
-    /* left circular shift */
+    /* Left circular shift */
     if(arg->i > 0)
         shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
             | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
-    /* right circular shift */
+    /* Right circular shift */
     else
         shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
             | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
