0dcbd07866164f19297ed5e0407ef4f298b0a7dc
Antonio Simone <antsimone03@gmail.com>
2021-05-01 11:33:59 +0200

    sans and dmenu tweaks

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const int showbar           = 1; /* 0 means no bar */
 static const int topbar            = 1; /* 0 means bottom bar */
 
 //static const char *fonts[]         = { "Terminus (TTF):size=11" };
-static const char *fonts[] = { "monospace:size=9" };
+static const char *fonts[] = { "sans:size=9" };
 
 static const char col_normfg[] = "#bcbcbc";
 static const char col_normbg[] = "#121212";
@@ -34,7 +34,7 @@ static const char *colors[][3] = {
     [SchemeStatus]   = { col_normfg, col_normbg, "#000000" },
     [SchemeTagsSel]  = { col_normbg, col_selbg, "#000000" },
     [SchemeTagsNorm] = { "#777777", col_normbg, "#000000" },
-    [SchemeInfoSel]  = { "#a991f1", col_normbg, "#000000" }, /* #5699af */
+    [SchemeInfoSel]  = { "#d0913d", col_normbg, "#000000" }, /* #5699af */
     [SchemeInfoNorm] = { col_normfg, col_normbg, "#000000" },
 };
 
@@ -48,7 +48,7 @@ static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },       /* first entry is default */
     { "[M]",      monocle },
-    { "><>",      NULL },       /* no layout function means floating behavior */
+    { "><>",      NULL },       /* no layut function means floating behavior */
 };
 
 /* Window rules */
