c16c506d2feb86a1d7e1acb059c78c5792bb9e30
Antonio Simone <antsimone03@gmail.com>
2021-05-12 08:55:24 +0200

    indent

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int showbar           = 1; /* 0 means no bar */
 static const int topbar            = 1; /* 0 means bottom bar */
 
 //static const char *fonts[]         = { "Terminus (TTF):size=11" };
-static const char *fonts[] = { "monospace:size=9" };
+static const char *fonts[] = { "monospace:size=11" };
 
 static const char col_normfg[] = "#bcbcbc";
 static const char col_normbg[] = "#121212";
